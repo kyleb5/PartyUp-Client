1326f684546dabe61a579c519eb62e0f3d48e5b3
Kyle B <kberry1298@gmail.com>
2024-02-21 17:55:13 -0600

    Complete Auth Setup

--- components/NavBar.js
+++ components/NavBar.js
@@ -14,7 +14,10 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>
+            {' '}
+            <img src="/partyup_logo.jpg" alt="Website Logo" height={64} width={64} draggable="false" />
+          </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -22,9 +25,6 @@ export default function NavBar() {
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/delete-me">
-              <Nav.Link>Delete Me</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>
               Sign Out
