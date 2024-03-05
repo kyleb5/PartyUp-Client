40a4339f81c233d9e515577c6486579d2cf91b9a
Kyle B <kberry1298@gmail.com>
2024-03-04 21:49:01 -0600

    Fixed new users not being able to do basic things unless refreshed page

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -12,37 +12,40 @@ export default function UserProfile() {
   const { user } = useAuth();
 
   useEffect(() => {
-    getUserFromFBKey(user.uid).then(setUserData);
-  }, [user.uid]);
+    getUserFromFBKey(user.fbKey).then(setUserData);
+  }, [user.fbKey, user.uid]);
 
-  const gravatarUrl = gravatar.url(userData.email_address, { s: '200', d: 'identicon', r: 'pg' });
+  const gravatarUrl = gravatar.url(userData?.email_address, { s: '200', d: 'identicon', r: 'pg' });
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '40vh' }}>
-      <Image src={gravatarUrl} alt="Gravatar" roundedCircle height={100} width={100} style={{ marginTop: '15px' }} />
-      <h2 style={{ marginBottom: '40px' }}>{userData.username}</h2>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '40vh' }}>
+        <title>Party Up | User Profile</title>
+        <Image src={gravatarUrl} alt="Gravatar" roundedCircle height={100} width={100} style={{ marginTop: '15px' }} />
+        <h2 style={{ marginBottom: '40px' }}>{userData?.username}</h2>
 
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-          <Image src="/discord.png" height={32} width={40} />
-          <div>{userData?.account_discord || 'Discord Username Not Provided'}</div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+            <Image src="/discord.png" height={32} width={40} />
+            <div>{userData.account_discord || 'Discord Username Not Provided'}</div>
+          </div>
+          <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+            <Image src="/playstation.png" height={32} width={40} />
+            <div>{userData.account_playstation || 'PlayStation Username Not Provided'}</div>
+          </div>
+          <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+            <Image src="/xbox.png" height={32} width={37} />
+            <div>{userData.account_xbox || 'Xbox Username Not Provided'}</div>
+          </div>
+          <div style={{ marginBottom: '10px', textAlign: 'center' }}>
+            <Image src="/steam.png" height={32} width={40} />
+            <div>{userData.account_steam || 'Steam Username Not Provided'}</div>
+          </div>
         </div>
-        <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-          <Image src="/playstation.png" height={32} width={40} />
-          <div>{userData?.account_playstation || 'PlayStation Username Not Provided'}</div>
-        </div>
-        <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-          <Image src="/xbox.png" height={32} width={37} />
-          <div>{userData?.account_xbox || 'Xbox Username Not Provided'}</div>
-        </div>
-        <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-          <Image src="/steam.png" height={32} width={40} />
-          <div>{userData?.account_steam || 'Steam Username Not Provided'}</div>
-        </div>
+        <Link passHref href="/edit-profile">
+          <Button variant="danger">Edit Profile</Button>
+        </Link>
       </div>
-      <Link passHref href="/edit-profile">
-        <Button variant="danger">Edit Profile</Button>
-      </Link>
-    </div>
+    </>
   );
 }
