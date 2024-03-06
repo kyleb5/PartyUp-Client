7dee02181bf374b316537c2b02a319cfb9223961
Kyle B <kberry1298@gmail.com>
2024-03-05 20:56:44 -0600

    Members profiles can now see profile pictures and CSS to make it look better

--- components/MembersCard.js
+++ components/MembersCard.js
@@ -1,7 +1,10 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState, useCallback } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'react-bootstrap/Image';
+import gravatar from 'gravatar';
 import { getUserInGroup, getSingleGroup, deleteGroupMember } from '../utils/data/groupData';
 import { useAuth } from '../utils/context/authContext';
 import { getUserFromFBKey } from '../utils/data/userData';
@@ -29,22 +32,28 @@ export default function MembersCard() {
     await fetchData();
   };
 
+  console.warn(groupDetails);
+
   return (
-    <div style={{ textAlign: 'center', listStylePosition: 'inside' }}>
-      <h6>Members:</h6>
-      <ul>
-        <li>Creator: {groupDetails?.uuid?.username}</li>
+    <div style={{ textAlign: 'center' }}>
+      <h6 style={{ marginRight: '11.5rem' }}>Members:</h6>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px', backgroundColor: '#252323' }}>
+          <Image src={gravatar.url(groupDetails?.uuid?.email_address, { s: '50', d: 'wavatar', r: 'pg' })} rounded />
+          <span style={{ minWidth: '13rem' }}>{groupDetails?.uuid?.username}</span>
+        </div>
         {members.map((member) => (
-          <li key={member.id}>
-            {member.user.username}
+          <div key={member.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '8px', backgroundColor: '#252323' }}>
+            <Image src={gravatar.url(member?.email_address, { s: '50', d: 'wavatar', r: 'pg' })} rounded />
+            <span style={{ minWidth: '13rem' }}>{member.user.username}</span>
             {userData?.id === groupDetails?.uuid?.id && (
-              <span style={{ cursor: 'pointer' }} onClick={() => handleRemoveMember(member)}>
+              <span style={{ cursor: 'pointer', marginLeft: '8px' }} onClick={() => handleRemoveMember(member)}>
                 🗑️
               </span>
             )}
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
